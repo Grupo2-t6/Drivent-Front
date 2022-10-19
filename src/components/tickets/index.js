9c8f5a6ceca8d5ec26fb5e8aed5b13dd0341748f
MariaDuda <mariabevenuto062@gmail.com>
2022-10-19 19:38:32 -0300

    feat: get ticket by id

--- src/components/tickets/index.js
+++ src/components/tickets/index.js
@@ -1,45 +1,15 @@
 import styled from 'styled-components';
-import ticketData from './ticketData';
 
 export default function Ticket({ value, type, uniqueValue, picked }) {
-  function pickTicket(uniqueValue) {
-    console.log(uniqueValue);
-    //mandar uma requisição lá no db e mudar os dados de picked pra true e criar o db lá
-    //dados não vão ser mais via ticketData 
-    //e ai renderizar o background de acordo com o picked ( se true vai ser vermelho se false branco)
-    // depois reduzir a seleção a um , então caso a pessoa clique no presencial , setar o online pra picked: false
-    //finalizar
-  }
   return (
-    <TicketsConteiner key={uniqueValue} onClick={pickTicket(uniqueValue)}>
-      <TickerBody>
-        <TicketContent>
-          <h2>{type}</h2>
-          <h4>{value}</h4>
-        </TicketContent>
-      </TickerBody>
-    </TicketsConteiner>
+    <TickerBody>
+      <TicketContent>
+        <h2>{type}</h2>
+        <h4>{value}</h4>
+      </TicketContent>
+    </TickerBody>
   );
 };
-
-const TicketsConteiner = styled.div`
-   width: 20%;
-   margin-top: 2%;
-
-  @media (max-width: 414px) {
-    width: 55%;
-  }
-  @media (max-width: 912px) {
-    width: 25%;
-  }
-  @media (max-width: 820px) {
-    width: 27%;
-  }
-  @media (max-width: 768px) {
-    width: 30%;
-  }
-
-`;
 
 const TickerBody = styled.div`
   width: 145px;
--- src/pages/Dashboard/Payment/index.js
+++ src/pages/Dashboard/Payment/index.js
@@ -4,19 +4,30 @@ import Ticket from '../../../components/tickets';
 import ticketData from '../../../components/tickets/ticketData';
 
 export default function Payment() {
+  const handleSelect = (elementIndex) => {
+    console.log(`You clicked on element with key ${elementIndex}`);
+
+    //mandar uma requisição lá no db e mudar os dados de picked pra true e criar o db lá
+    //dados não vão ser mais via ticketData 
+    //e ai renderizar o background de acordo com o picked ( se true vai ser vermelho se false branco)
+    // depois reduzir a seleção a um , então caso a pessoa clique no presencial , setar o online pra picked: false
+    //finalizar
+  };
   return (
     <PaymentConteiner>
       <h1>Ingresso e pagamento</h1>
       <h3>Primeiro, escolha sua modalidade de ingresso</h3>
-      <div>
+      <div >
         {ticketData.map((e, index) => {
           return(
-            <Ticket
-              picked={e.picked} 
-              uniqueValue={index}
-              value={e.value}
-              type={e.type}
-            />
+            <TicketsConteiner onClick={e => handleSelect(index)}>
+              <Ticket
+                picked={e.picked} 
+                uniqueValue={index}
+                value={e.value}
+                type={e.type}
+              />
+            </TicketsConteiner>
           );
         })}
       </div>
@@ -46,5 +57,11 @@ const PaymentConteiner = styled.div`
   }
   div{
     display: flex;
+    width: 75%;
   }
 `;
+
+const TicketsConteiner = styled.div`
+   margin-top: 2%;
+
+`;  
