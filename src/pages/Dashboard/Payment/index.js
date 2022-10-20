eb4554459a47c210a81a7abf279ce5afb09e774a
MariaDuda <mariabevenuto062@gmail.com>
2022-10-20 14:11:12 -0300

    feat: select hotel

--- src/pages/Dashboard/Payment/index.js
+++ src/pages/Dashboard/Payment/index.js
@@ -1,37 +1,72 @@
+import { set } from 'date-fns';
 import { useState } from 'react';
 import styled from 'styled-components';
 import Ticket from '../../../components/tickets';
 import ticketData from '../../../components/tickets/ticketData';
+import withHotelData from '../../../components/tickets/hotelData';
 
 export default function Payment() {
   const [selected, setSelected] = useState(ticketData);
+  const [selectedHotel, setSelectedHotel] = useState(withHotelData);
   const [ispicked, setIspicked] = useState(false);
-  const handleSelect = (elementIndex) => {
+  const handleSelect = (elementIndex, object, keyIndex) => {
     let newData = [];
-    ticketData.map((value, index) => {
-      if(index === elementIndex) {
+    let hotelNewData = [];
+    object.map((value, index) => {
+      if(index === elementIndex && keyIndex === elementIndex + 1) {
         const data = {
           ...value,
           picked: true
+        };
+        setIspicked(true);
+        newData.push(data);
+      }else if (index === elementIndex && keyIndex === elementIndex + 3) {
+        const data = {
+          ...value,
+          picked: true
+        };
+        hotelNewData.push(data);
+      }else if(index !== elementIndex && keyIndex === elementIndex + 1) {
+        const data = {
+          ...value
         };
         newData.push(data);
       }else{
         const data = {
           ...value
         };
-        newData.push(data);
+        hotelNewData.push(data);
       }
     });
-    setSelected(newData);
+    if(keyIndex === elementIndex + 3) setSelectedHotel(hotelNewData);
+    if(keyIndex === elementIndex + 1) setSelected(newData);
   };
+
   return (
     <PaymentConteiner>
       <h1>Ingresso e pagamento</h1>
       <h3>Primeiro, escolha sua modalidade de ingresso</h3>
-      <div >
+      <div>
         {selected.map((e, index) => {
+          let code = e.key; 
           return(
-            <TicketsConteiner onClick={e => handleSelect(index)}>
+            <TicketsConteiner onClick={e => handleSelect(index, ticketData, code)}>
+              <Ticket
+                picked={e.picked} 
+                uniqueValue={index}
+                value={e.value}
+                type={e.type}
+              />
+            </TicketsConteiner>
+          );
+        })}
+      </div>
+      <h3>Ótimo! Agora escolha sua modalidade de hospedagem</h3>
+      <div>
+        {selectedHotel.map((e, index) => {
+          let code = e.key; 
+          return(
+            <TicketsConteiner onClick={e => handleSelect(index, withHotelData, code)}>
               <Ticket
                 picked={e.picked} 
                 uniqueValue={index}
