57c151cd91f6cca3892e13bc032de698765fc486
= <=>
2022-11-04 15:29:49 -0300

    fix oAuth

--- src/pages/Dashboard/FillSubscription/index.js
+++ src/pages/Dashboard/FillSubscription/index.js
@@ -1,6 +1,27 @@
 import PersonalInformationForm from '../../../components/PersonalInformationForm';
+import api from '../../../services/api';
+import UserContext from '../../../contexts/UserContext';
+import { useState, useContext } from 'react';
 
 export default function FillSubscription() {
+  const { setUserData, userData } = useContext(UserContext);
+
+  const params = new Proxy(new URLSearchParams(window.location.search), {
+    get: (searchParams, prop) => searchParams.get(prop),
+  });
+  let code = params.code;
+  signIn(code);
+  
+  async function signIn(code) {
+    if(code) {
+      const response = await api.post('auth/sign-in-oauth', { code });
+      const token = response.data;
+      const userData = { token };
+      setUserData(userData);
+      return response.data;
+    };
+  }
+
   return (
     <PersonalInformationForm />
   );
--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -14,6 +14,8 @@ import UserContext from '../../contexts/UserContext';
 
 import useSignIn from '../../hooks/api/useSignIn';
 
+import qs from 'query-string';
+
 export default function SignIn() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
@@ -36,8 +38,21 @@ export default function SignIn() {
     } catch (err) {
       toast('Não foi possível fazer o login!');
     }
-  } 
+  }
 
+  function redirectToGithub() {
+    const GITHUB_AUTH_URL = 'https://github.com/login/oauth/authorize';
+    const params = {
+      response_type: 'code',
+      scope: 'user public_repo',
+      client_id: '8ffd828e28e1aa67ba4f',
+    };
+  
+    const queryStrings = qs.stringify(params);
+    const authorizationUrl = `${GITHUB_AUTH_URL}?${queryStrings}`;
+    window.location.href = authorizationUrl;
+  }
+    
   return (
     <AuthLayout background={eventInfo.backgroundImageUrl}>
       <Row>
@@ -52,6 +67,7 @@ export default function SignIn() {
           <Button type="submit" color="primary" fullWidth disabled={loadingSignIn}>Entrar</Button>
         </form>
       </Row>
+      <Button type="submit" color="primary" onClick={redirectToGithub} >Entre com o GitHub</Button>
       <Row>
         <Link to="/enroll">Não possui login? Inscreva-se</Link>
       </Row>
